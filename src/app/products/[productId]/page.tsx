becad7d16195430fc6f246a228a9fd00176ebf82
saad-mamun <mamunali88991@gmail.com>
2025-05-06 16:14:05 +0600

    metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,29 @@
-export default async function ProductsDetails({
+// Dynamic meta data update
+
+import { Metadata } from "next";
+
+type Props = {
+    params: Promise<{productId: string}>
+}
+
+export const generatedMetadata = async ({
     params,
-}: {
-    params : Promise<{ productId: string}>;
-}){
+}: Props): Promise<Metadata> => {
+    const id = (await params).productId;
+    const title = await new Promise((resolve) =>{
+        setTimeout(() => {
+            resolve(`Products ${id}`)
+        }, 100);
+    })
+    return {
+        title: `Products ${title}`
+    }
+}
+
+
+
+export default async function ProductsDetails({params}: Props){
+
    const productId = (await params).productId;
    
    return (
